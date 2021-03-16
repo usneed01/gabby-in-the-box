3d6fffc0826b510fbee44bba044315be126ea048
Ulyssa Sneed <ulyssasneed@Ulyssas-MBP.attlocal.net>
2021-03-16 00:31:49 -0400

    java

---
+++ contact.js
@@ -0,0 +1,9 @@
+function validation(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+
+    error_message.style.padding = "12px";
+}
